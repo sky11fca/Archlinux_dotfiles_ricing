23a1dc18968aed94684cfeaf5c777308cb5a7bd1
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2024-12-05 13:58:02 +0200

    IceWM configurations

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,109 +1,165 @@
-local vim = vim
-local Plug = vim.fn['plug#']
-vim.call('plug#begin')
+--local vim = vim
+--local Plug = vim.fn['plug#']
 
-	Plug('vim-airline/vim-airline')
-	Plug('vim-airline/vim-airline-themes')
-	Plug('ryanoasis/vim-devicons')
-	Plug('ap/vim-css-color')
-	Plug('neoclide/coc.nvim', {['branch'] = 'release'})
+
+--vim.call('plug#begin')
+
+--	Plug('vim-airline/vim-airline')
+--	Plug('vim-airline/vim-airline-themes')
+--	Plug('ryanoasis/vim-devicons')
+--	Plug('ap/vim-css-color')
+--	Plug('neoclide/coc.nvim', {['branch'] = 'release'})
 --	Plug('preservim/nerdtree')
-	Plug('bluz71/vim-nightfly-colors', {['as'] = 'nightfly'})
-	Plug('lambdalisue/battery.vim')
-	Plug('ervandew/supertab')
-	Plug('junegunn/fzf.vim')
-	Plug('tpope/vim-fugitive')
-	Plug('christoomey/vim-tmux-navigator')
+--	Plug('bluz71/vim-nightfly-colors', {['as'] = 'nightfly'})
+--	Plug('lambdalisue/battery.vim')
+--	Plug('ervandew/supertab')
+--	Plug('junegunn/fzf.vim')
+--	Plug('tpope/vim-fugitive')
+--	Plug('christoomey/vim-tmux-navigator')
 --	Plug('startup-nvim/startup.vim')
-	Plug('nvim-tree/nvim-tree.lua')
+--	Plug('nvim-tree/nvim-tree.lua')
 --	Plug('tc50cal/vim-terminal')
-
+--vim.call('plug#end')
 --vim.cmd [[packadd packer.vim]]
-require('packer').startup(function(use)
+--require('packer').startup(function(use)
 
-	 use ('wbthomason/packer.nvim')
+--	 use ('wbthomason/packer.nvim')
 
-	use ({
-  	"startup-nvim/startup.nvim",
-  	requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim"},
-  	config = function()
-    	require"startup".setup()
- 	 end})
-	 use {
-    'maxmx03/solarized.nvim',
-    config = function()
-      vim.o.background = 'dark'
+--	use ({
+--  	"startup-nvim/startup.nvim",
+--  	requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim"},
+--  	config = function()
+--    	require"startup".setup()
+-- 	 end})
+--	 use {
+  --  'maxmx03/solarized.nvim',
+    --config = function()
+      --vim.o.background = 'dark'
       ---@type solarized
-      local solarized = require('solarized')
-      vim.o.termguicolors = true
-      vim.o.background = 'dark'
-      solarized.setup({})
-      vim.cmd.colorscheme 'solarized'
-    end
-}
-	 use ( "calind/selenized.nvim" )
-	 use ("EdenEast/nightfox.nvim")
+     -- local solarized = require('solarized')
+     -- vim.o.termguicolors = true
+      --vim.o.background = 'dark'
+    --  solarized.setup({})
+      --vim.cmd.colorscheme 'solarized'
+   -- end
+--}
+--	 use ( "calind/selenized.nvim" )
+--	 use ("EdenEast/nightfox.nvim")
 --	 use ('neoclide/coc.nvim')
-	 use ("nvim-telescope/telescope.nvim")
-	 use ('rcarriga/nvim-notify')
-	 use ('nvim-tree/nvim-web-devicons') -- OPTIONAL: for file icons
-	 use ('lewis6991/gitsigns.nvim') -- OPTIONAL: for git status
-	 use ('romgrk/barbar.nvim')
-	 use('nvim-tree/nvim-tree.lua')
-	 use ('hrsh7th/nvim-cmp')
-	 use ('hrsh7th/cmp-nvim-lsp')
-	 use {
-    		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig"
-	 }
-end)
+--	 use ("nvim-telescope/telescope.nvim")
+--	 use ('rcarriga/nvim-notify')
+--	 use ('nvim-tree/nvim-web-devicons') -- OPTIONAL: for file icons
+--	 use ('lewis6991/gitsigns.nvim') -- OPTIONAL: for git status
+--	 use ('romgrk/barbar.nvim')
+--	 use('nvim-tree/nvim-tree.lua')
+--	 use ('hrsh7th/nvim-cmp')
+--	 use ('hrsh7th/cmp-nvim-lsp')
+--	 use {
+  --  		"williamboman/mason.nvim",
+--		"williamboman/mason-lspconfig.nvim",
+--		"neovim/nvim-lspconfig"
+--	 }
+--	 use {
+--    		"nvim-telescope/telescope-file-browser.nvim",
+--    		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+--	}
+--end)
 
-vim.call('plug#end')
+--vim.call('plug#end')
 
---vim.g['battery#update_tabline'] = 1
---vim.g['airline#extensions#battery#enabled'] = 1
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo="https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({"git","clone","--filter=blob:none", "--branch=stable",lazyrepo, lazypath})
+end
 
---vim.g.airline_left_sep = ''
---vim.g.airline_right_sep = ''
---vim.g.airline_left_alt_sep = ''
---vim.g.airline_right_alt_sep = ''
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
 
+	{
+		'maxmx03/solarized.nvim',
+		lazy=false,
+		priority=1000,
+		---@type solarized.config
+		opts={},
 
-vim.g.airline_powerline_fonts = 1
---vim.g['airline#extensions#tabline#enabled'] = 1
---vim.g['airline#extensions#tabline#left_sep'] = ' '
---vim.g['airline#extensions#tabline#left_alt_sep'] = '/'
---vim.g['airline#extensions#tabline#formatter'] = 'unique_tail'
+		config=function(_, opts)
+			vim.o.termguicolors=true
+			vim.o.background = 'dark'
+			require('solarized').setup(opts)
+			vim.cmd.colorscheme 'solarized'
+		end,
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies={'nvim-tree/nvim-web-devicons'}
+	},
 
-vim.g.airline_theme='solarized'
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+	{
+		'romgrk/barbar.nvim',
+		config = function()
+			require("barbar").setup()
+		end
+	},
+	{
+		'nvim-telescope/telescope.nvim'
+	},
+	{
+		'rcarriga/nvim-notify'
+	},
+	{
+		'christoomey/vim-tmux-navigator',
+		vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", opts),
+		vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", opts),
+		vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", opts),
+		vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", opts),
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+    		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+	},
+	{
+		'startup-nvim/startup.nvim',
+		config = function()
+			require("startup").setup({theme = "startify"})
+		end
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+    		branch = "v3.x",
+    		dependencies = {
+      			"nvim-lua/plenary.nvim",
+      			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      			"MunifTanjim/nui.nvim",
+      			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    		},
 
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
+		config = function()
+			filesystem = {
+			hide_dotfiles = false,
+			hide_hidden = false
 
--- empty setup using defaults
-require("nvim-tree").setup()
+			}
+		end
+	},
 
--- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate",
+		
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+      			configs.setup({
+          			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "sql", "bash", "dockerfile" },
+          			sync_install = false,
+          			highlight = { enable = true },
+          			indent = { enable = true },  
+        		})
+		end
+
+	}
+
 })
 
-require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "rust_analyzer" },
-}
+require('lualine').setup()
--- .zshrc
+++ .zshrc
@@ -1,11 +1,11 @@
 # Created by newuser for 5.9
 
 alias ls='eza --color=auto'
-alias grep='grep --color=auto'
+alias grep='rg --color=auto'
 alias cat='bat'
 alias cd='z'
 alias v='nvim'
-alias nf='fastfetch --logo arch2'
+alias neofetch='fastfetch'
 alias ll='eza -lah'
 alias f='"$(fzf)"'
 alias swaylock='swaylock --color #000000'
@@ -18,13 +18,28 @@ alias doc='nvim $(find ~/Documents/ -type f | fzf)'
 alias dotf='cd $DOTFILES'
 
 alias notes='nvim ~/Notes/'
+#RPROMPT='%F{blue}λ%f'
+#RPROMPT="%F{blue}[%D %t]%f"
+#PS1="%F{cyan}╔[󰣇]═[%f%F{blue}%n%f%F{white}@%f%F{cyan}%m]═[%f%F{magenta}%1~%f%F{cyan}]%f"$'\n'"%F{blue}╚[%#]═>%f "
+#PS2="═[+]"
 
-RPROMPT="%F{blue}[%D %t]%f"
-PS1="%F{cyan}╔[󰣇]═[%f%F{blue}%n%f%F{white}@%f%F{cyan}%m]═[%f%F{magenta}%1~%f%F{cyan}]%f"$'\n'"%F{blue}╚[%#]═>%f "
-PS2="═[+]"
+autoload -Uz vcs_info
+precmd() {vcs_info}
+zstyle ':vcs_indo:*' formats ' %s(%b)'
+
+
+PS1="%F{cyan}%~%f"$'\n'"%F{blue}> %f"
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory
 
 #powerline-daemon -q
 #. /usr/share/powerline/bindings/zsh/powerline.zsh
 
 source <(fzf --zsh)
 eval "$(zoxide init zsh)"
+
+
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
